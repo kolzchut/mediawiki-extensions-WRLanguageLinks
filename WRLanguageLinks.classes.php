6661ad29af1d44f9046e69008be93ffcd1ee7565
Dror S <FreedomFighterSparrow@kolzchut.org.il>
2014-03-10 17:24:48 +0200

    Upgrade to MW 1.22.3 (FROM 1.21.6)
    Including upgrades of extensions, where appropriate.

--- WRLanguageLinks.classes.php
+++ WRLanguageLinks.classes.php
@@ -1,70 +1,70 @@
 <?php
 /**
-* Classes for WRLanguageLinks extension
-*
-* @file
-* @ingroup Extensions
-*/
+ * Classes for WRLanguageLinks extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
 
 // WRLanguageLinks class
 class WRLanguageLinks {
-	/* Fields */
-	
-	private $mParser;
-	const markerText = 'x--WRLanguageLinks-marker--x';
-	const markerHasLinksText = 'x--WRLanguageLinks-has-links-marker--x';
-	
-	/* Functions */ 
-	
-	public function __construct( Parser $parser ) {
-		$this->mParser = $parser;
-	}
-	
-	public static function renderMarker() {
-		return self::markerText;
-	}
-	
-	public static function renderHasLinksMarker() {
-		return self::markerHasLinksText;
-	}
-	
-	public function render( &$text ) {
-		// find markers in $text and replace with actual output
-		$text = str_replace( self::markerText, $this->getLanguageLinks(), $text ); // Find markers for language links
-		$text = str_replace( self::markerHasLinksText, $this->hasLanguageLinks(), $text);	  // Find markers for hasLanguageLinks	
-		return true;
-	}
-	
-	private function hasLanguageLinks() {
-		global $wgWRLanguageLinksShowOnly;
-		$parserLanguageLinks = $this->mParser->getOutput()->getLanguageLinks();
-		$showOnly = explode( ',', $wgWRLanguageLinksShowOnly );
-		if( $parserLanguageLinks == null ) {
-			return "false"; // no language links at all, valid or otherwise
-		} elseif( count( $showOnly ) == 0 ) {
-			return "true"; // there are some language links, and all links are considered valid
-		} else {
-			foreach( $parserLanguageLinks as $l ) {
-				$tmp = explode( ':', $l, 2 );
-				if( in_array( $tmp[0], $showOnly ) ) return "true";	// there is at least one valid language link
-			}
-		}
-		return "false"; // if we got this far, there are no valid language links
-	}
-	
-	private function makeLanguageLinks() {
-		global $wgWRLanguageLinksShowOnly, $wgWRLanguageLinksShowTitles;
+    /* Fields */
 
-		// Language links - ripped from SkinTemplate.php and then mangled badly
-		$parserLanguageLinks = $this->mParser->getOutput()->getLanguageLinks();
-		$language_urls = array();
+    private $mParser;
+    const markerText = 'x--WRLanguageLinks-marker--x';
+    const markerHasLinksText = 'x--WRLanguageLinks-has-links-marker--x';
+
+    /* Functions */
+
+    public function __construct( Parser $parser ) {
+        $this->mParser = $parser;
+    }
+
+    public static function renderMarker() {
+        return self::markerText;
+    }
+
+    public static function renderHasLinksMarker() {
+        return self::markerHasLinksText;
+    }
+
+    public function render( &$text ) {
+        // find markers in $text and replace with actual output
+        $text = str_replace( self::markerText, $this->getLanguageLinks(), $text ); // Find markers for language links
+        $text = str_replace( self::markerHasLinksText, $this->hasLanguageLinks(), $text);	  // Find markers for hasLanguageLinks
+        return true;
+    }
+
+    private function hasLanguageLinks() {
+        global $wgWRLanguageLinksShowOnly;
+        $parserLanguageLinks = $this->mParser->getOutput()->getLanguageLinks();
+        $showOnly = explode( ',', $wgWRLanguageLinksShowOnly );
+        if( $parserLanguageLinks == null ) {
+            return "false"; // no language links at all, valid or otherwise
+        } elseif( count( $showOnly ) == 0 ) {
+            return "true"; // there are some language links, and all links are considered valid
+        } else {
+            foreach( $parserLanguageLinks as $l ) {
+                $tmp = explode( ':', $l, 2 );
+                if( in_array( $tmp[0], $showOnly ) ) return "true";	// there is at least one valid language link
+            }
+        }
+        return "false"; // if we got this far, there are no valid language links
+    }
+
+    private function makeLanguageLinks() {
+        global $wgWRLanguageLinksShowOnly, $wgWRLanguageLinksShowTitles;
+
+        // Language links - ripped from SkinTemplate.php and then mangled badly
+        $parserLanguageLinks = $this->mParser->getOutput()->getLanguageLinks();
+        $language_urls = array();
         $showOnly = array();
 
         if( $wgWRLanguageLinksShowOnly != null ) {
             $showOnly = explode( ',', $wgWRLanguageLinksShowOnly );
         }
 
-		foreach( $parserLanguageLinks as $languageLinkText ) {
+        foreach( $parserLanguageLinks as $languageLinkText ) {
             $languageLinkTitle = Title::newFromText( $languageLinkText );
             if ( $languageLinkTitle ) {
                 $ilInterwikiCode = $languageLinkTitle->getInterwiki();
@@ -98,7 +98,7 @@ class WRLanguageLinks {
         $hasSingleLink = ( count( $languageLinks ) === 1 );
 
         $class = 'wr-languagelinks' . ( $hasSingleLink ? ' single-link' : '' );
-        $output = '<div class="' . $class .'"><span class="wr-languagelinks-title">';
+        $output = '<li><div class="' . $class .'"><span class="wr-languagelinks-title">';
 
         if( $hasSingleLink ) {
             $output .= wfMessage( 'wr-in-single-language', $languageLinks[0]['lang'] )->inContentLanguage()->text();
@@ -115,13 +115,13 @@ class WRLanguageLinks {
             $output .= '<li class="'. htmlspecialchars(  $langLink['class'] ) . '">' .
                 '<a href="' . htmlspecialchars( $langLink['href'] ) . '" ' .
                 'title="' . htmlspecialchars( $langLink['title'] ) . '">' . $langLink['text'] . '</a></li>';
-		}
+        }
 
-        $output .= '</ul></div>';
+        $output .= '</ul></div></li>';
         //$this->mParser->getOutput()->addModules( 'ext.WRLanguageLinks' );
 
-		return $output;
-	}
+        return $output;
+    }
 
 }
 
