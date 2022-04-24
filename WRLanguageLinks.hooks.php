5f11a5b2abda1910fdc2d1f624c779709b77a019
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2022-04-24 13:38:35 +0300

    Create and add wgLanguageLinks to js config
    This allows other code to retrieve language links, including full links, on the client side.

--- WRLanguageLinks.hooks.php
+++ WRLanguageLinks.hooks.php
@@ -28,4 +28,27 @@ class WRLanguageLinksHooks {
 		// Return output
 		$wrLanguageLinks->render( $text );
 	}
+
+	/**
+	 * MakeGlobalVariablesScript hook handler
+	 * For values that depend on the current page, user or request state.
+	 *
+	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/MakeGlobalVariablesScript
+	 * @param array &$vars Variables to be added into the output
+	 * @param OutputPage $out instance calling the hook
+	 */
+	public static function onMakeGlobalVariablesScript( array &$vars, OutputPage $out ) {
+		$langLinks = $out->getLanguageLinks();
+		$langLinksArray = [];
+
+		foreach ( $langLinks as $link ) {
+			$explodedLink = explode( ':', $link, 2 );
+			$langLinksArray[ $explodedLink[0] ] = [
+				'title' => $explodedLink[1],
+				'url' => Title::newFromText( $link )->getFullURL()
+			];
+		}
+
+		$vars['wgLanguageLinks'] = $langLinksArray;
+	}
 }
